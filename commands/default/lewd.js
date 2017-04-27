daca4d820259c7e77cc0e58173ee791cb7fa51f6
Dylan Lau <drlau@uwaterloo.ca>
2017-04-26 22:45:46 -0700

    added lewd reaction command

---
+++ commands/default/lewd.js
@@ -0,0 +1,8 @@
+exports.cmd = new Mage.Command("lewd", async message => {
+  let images = await Magic.getFiles("./img/lewd/");
+  index = Magic.randomInt(0, images.length);
+  Magic.sendImage(images[index], message);
+}, {
+	type: "default",
+	description: "2 lewd 4 me"
+});
