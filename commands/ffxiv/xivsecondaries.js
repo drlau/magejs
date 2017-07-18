7264da64b8f586674220992c88f138b24f5bd892
Dylan Lau <drlau@uwaterloo.ca>
2017-07-18 11:48:12 -0400

    typo

--- commands/ffxiv/xivsecondaries.js
+++ commands/ffxiv/xivsecondaries.js
@@ -4,10 +4,10 @@ exports.cmd = new Mage.Command("secondaries", async message => {
   output = "";
   output += "Information based on data found here: https://goo.gl/f2Bk9u\n";
   output += "This is a rule of thumb, not stat weights. Stat weights take into account class mechanics (i.e. BRD and MNK favour crit for procs)\n\n";
-  output += "Direct Hit > Det = SS > Crit > Tenacity\n";
+  output += "DH > Det = SS > Crit > Tenacity\n";
   output += "Crit > Tenacity starting at around 700\n"
   output += "Crit > Det/SS starting at around 1500\n"
-  output += "Crit > DHR starting at around 1900"
+  output += "Crit > DH starting at around 1900"
 
   message.channel.createMessage(output);
 }, {
