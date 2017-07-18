ff4695f616df6debfb6a0ba3732354ee1bdc5bfb
Dylan Lau <drlau@uwaterloo.ca>
2017-07-18 11:40:04 -0400

    secondary info

---
+++ commands/ffxiv/xivsecondaries.js
@@ -0,0 +1,16 @@
+const patch = "3.4";
+
+exports.cmd = new Mage.Command("secondaries", async message => {
+  output = "";
+  output += "Information based on data found here: https://goo.gl/f2Bk9u\n";
+  output += "This is a rule of thumb, not stat weights. Stat weights take into account class mechanics (i.e. BRD and MNK favour crit for procs)\n\n";
+  output += "Direct Hit > Det = SS > Crit > Tenacity\n";
+  output += "Crit > Tenacity starting at around 700\n"
+  output += "Crit > Det/SS starting at around 1500\n"
+  output += "Crit > DHR starting at around 1900"
+
+  message.channel.createMessage(output);
+}, {
+	type: "default",
+	description: "Displays information about secondary stats in FFXIV."
+});
