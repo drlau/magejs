9cb4918345f53727af8baba31384571c872b5fcf
Dylan Lau <drlau@uwaterloo.ca>
2017-09-17 00:27:13 -0700

    new author command to change game

---
+++ commands/author/game.js
@@ -0,0 +1,22 @@
+exports.cmd = new Mage.Command("game", async message => {
+  if (message.author.id != Magic.config.author){
+    return;
+  }
+
+  let authorDM = await bot.getDMChannel(Magic.config.author);
+  if (message.channel != authorDM) return;
+
+  let game = "";
+
+  if (!message.args[0]) {
+    message.channel.createMessage("Aidil, don't forget the game!");
+    return;
+  }
+
+  game = message.args.join(" ");
+  bot.editStatus("online", {name: game})
+  message.channel.createMessage("I am now playing " + game + "!");
+}, {
+	type: "author",
+	description: "Sets Mage's game. Author only."
+});
