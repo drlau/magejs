f1856e374e89c69c55f86dc09296c988d2fb0d04
Dylan Lau <drlau@uwaterloo.ca>
2019-02-07 23:23:23 -0500

    add set xiv patch

---
+++ commands/author/setxivpatch.js
@@ -0,0 +1,22 @@
+exports.cmd = new Mage.Command("setxivpatch", async message => {
+  if (message.author.id != Magic.config.author){
+    return;
+  }
+
+  let authorDM = await bot.getDMChannel(Magic.config.author);
+  if (message.channel != authorDM) return;
+
+  let game = "";
+
+  if (!message.args[0]) {
+    message.channel.createMessage("I need a patch number!");
+    return;
+  }
+
+  xivPatch = message.args[0];
+  vars.setXivPatch(xivPatch);
+  message.channel.createMessage("XIV Patch set to " + xivPatch + "!");
+}, {
+	type: "author",
+	description: "Sets the FFXIV Patch Number. Author only."
+});
